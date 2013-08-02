f4b0d40ba46fbe84961ab7753d5521a24c4119ff
Matt Johnson <mjohn23@gmail.com>
2013-08-02 17:18:19 +0000

    Add models and admin

--- membership/models.py
+++ membership/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now_add=True, editable=False)
+    name = models.CharField(max_length=255)
+
+class Member(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now_add=True, editable=False)
+    shop = models.ForeignKey(Shop)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    start_date = models.DateTimeField('membership started')
+    
