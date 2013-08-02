0c3a20d925dffae61ccb2b055844c9c9fbcec0d4
Matt Johnson <mjohn23@gmail.com>
2013-08-02 17:25:22 +0000

    Add basic member information

--- membership/admin.py
+++ membership/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from membership.models import Shop
+from membership.models import Shop, Member
 
 admin.site.register(Shop)
+admin.site.register(Member)
--- membership/models.py
+++ membership/models.py
@@ -5,6 +5,9 @@ class Shop(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True, editable=False)
     name = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return self.name
+
 class Member(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now_add=True, editable=False)
@@ -13,4 +16,7 @@ class Member(models.Model):
     last_name = models.CharField(max_length=50)
     middle_name = models.CharField(max_length=50)
     start_date = models.DateTimeField('membership started')
+
+    def __unicode__(self):
+        return self.last_name + ", " + self.first_name
     
